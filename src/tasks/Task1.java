66e7d5a2f5dbf46f60e095981c4b626cc2846e8e
m1dnas <felipesandimguliermone@gmail.com>
2023-06-03 03:07:50 -0300

    task 1

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package java;
-
+package tasks;
+import java.util.Scanner;
 /**
  *
  * @author Felipe
